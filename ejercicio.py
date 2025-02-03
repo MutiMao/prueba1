2ac2a499898ab64043f30b4b7583bf261a9bd4b4
MutiMao <garciagramaurou@gmail.com>
2025-02-03 08:22:25 -0500

    first commit

---
+++ ejercicio.py
@@ -0,0 +1,4 @@
+print("Hola Mundo")
+nombre = input("Ingrese su nombre: ")
+apellido = input ("Ingrese su apellido: ")
+print(f"Hola {nombre} {apellido}")
---
+++ suma.py
@@ -0,0 +1,5 @@
+print("Haremos una suma")
+numero1 = int(input("Ingrese número 1: "))
+numero2 = int(input("Ingrese número 2: "))
+suma = numero1 + numero2
+print(f"El resultado de la suma es = {suma}")
